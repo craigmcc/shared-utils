c44ea95d3ccb9401c14bdfe3fa4ebb910828e364
Craig McClanahan <craigmcc@gmail.com>
2022-04-23 12:32:11 -0700

    Add Timestamps module.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {default as Dates} from "./Dates";
 export {default as Months} from "./Months";
+export {default as Timestamps} from "./Timestamps";
 export {default as Validators} from "./Validators";
